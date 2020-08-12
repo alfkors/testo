23ecdcd83560c6bb3db52556556ab1e1bea16ea3
alfkors <aleshchanov@gmail.com>
2020-08-12 16:43:44 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,19 @@
 node default {
+  file { '/root/README':
+    ensure  => present,
+    content => 'Life is good!',
+    owner   => 'root',
+  }
 }
+
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
