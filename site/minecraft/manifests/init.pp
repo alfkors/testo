26afb279a261ce2bbdef78677bbeb4accf41ccd2
alfkors <aleshchanov@gmail.com>
2020-08-13 17:07:23 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -23,7 +23,9 @@ class minecraft (
   
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service',{
+      install_dir => $install_dir,
+    }),
   }
   
   service { 'minecraft':
